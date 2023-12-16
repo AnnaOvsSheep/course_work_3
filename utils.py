245b15af2033f38c0fddc27540bba9501784d359
AnnaOvsSheep <x180475@yandex.ru>
2023-12-16 15:01:54 +0300

    Реализация проекта

---
+++ utils.py
@@ -0,0 +1,43 @@
+import json
+
+from classes.operations_class import Operation
+
+
+def get_list_operations(path):
+    """
+    Функция вызова списка операций
+    :param path: путь к файлу
+    :return: список операций
+    """
+    with open(path, encoding="utf-8") as file:
+        return json.load(file)
+
+
+def get_operations(operations):
+    """
+    Функция вызова выполненных операций из списка операций
+    :param operations: список операций
+    :return: экземпляр класса Operation
+    """
+    all_operations = []
+    for operation in operations:
+        if operation and operation['state'] == 'EXECUTED':
+            operation = Operation(
+                pk=operation['id'],
+                state=operation['state'],
+                date=operation['date'],
+                operation_amount=operation['operationAmount'],
+                description=operation['description'],
+                whence=operation.get('from', ''),
+                to=operation['to']
+            )
+            all_operations.append(operation)
+    return all_operations
+
+def get_operation_date(operation):
+    """Вызов даты операции"""
+    return operation.date
+
+def sort_by_date(operations):
+    """Сортировка списка операций по дате"""
+    return sorted(operations, key=get_operation_date, reverse=True)
